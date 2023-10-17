8232edd7715db0dc993d2e76efeb84b5385dbdfe
Aditya Oberai <adityaoberai1@gmail.com>
2023-10-17 14:42:51 +0000

    Add db function

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { OpenAIApi, Configuration } from 'openai';
 import { getStaticFile, throwIfMissing } from './utils.js';
+import { Client, Databases, ID } from 'node-appwrite';
 
 export default async ({ req, res }) => {
-  throwIfMissing(process.env, ['OPENAI_API_KEY']);
+  throwIfMissing(process.env, ['OPENAI_API_KEY', 'APPWRITE_API_KEY']);
+
+  const appwriteClient = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('mlhghw')
+    .setKey(process.env.APPWRITE_API_KEY);
+
+  const appwriteDatabases = new Databases(appwriteClient);
 
   if (req.method === 'GET') {
     return res.send(getStaticFile('index.html'), 200, {
@@ -29,8 +37,22 @@ export default async ({ req, res }) => {
       messages: [{ role: 'user', content: req.body.prompt }],
     });
     const completion = response.data.choices[0].message?.content;
+
+    log(completion);
+
+    await appwriteDatabases.createDocument(
+      databaseId = 'testdb',
+      collectionId = 'testcoll',
+      documentId = ID.unique(),
+      data = {
+        prompt: req.body.prompt,
+        answer: completion
+      }
+    );
+
     return res.json({ ok: true, completion }, 200);
   } catch (err) {
+    error(err.message);
     return res.json({ ok: false, error: 'Failed to query model.' }, 500);
   }
 };
